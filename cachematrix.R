9669b763dcdd82d496d69a17cc5b49ac978954eb
nsutton <neil.t.sutton@gmail.com>
2015-03-20 12:13:57 -0400

    Initial Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Using caching to store repeatedly used data for better efficiency
 
-## Write a short comment describing this function
+## Returns a list of functions Set, get, setinvm, getinvm
 
 makeCacheMatrix <- function(x = matrix()) {
-
+       
+             
+        invm = NULL
+        set = function(y) {
+                x <<- y
+                inv_m <<- NULL
+        }
+        get = function() x
+        setinvm = function(m) invm <<- m 
+        getinvm = function() invm
+        list(set=set, get=get, setinvm=setinvm, getinvm=getinvm)
 }
 
 
-## Write a short comment describing this function
+## Gets the inverse of a supplied matrix, but first checked to see if 
+## the data has first been cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        invmatrix <- x$getinvm()
+        ##If it is cached
+        if (!is.null(invmatrix)){
+               
+                return(invmatrix)
+        }
+        # if not cached Created the cached version.
+        mdata <- x$get()
+        invmatrix <- solve(mdata)
+                
+        x$setinvm(invmatrix)
+        
+        return(invmatrix)
 }
